6ba5abb0f6b04b6735ad503f166e5847865f7fad
ian.dover <ian.dover@skunova.com>
2024-04-20 18:06:13 +0000

    simplified timeline code

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -3,14 +3,49 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def bio():
-    return render_template('bio.html')
 
-@app.route('/timeline')
+@app.route("/")
+def bio():
+    return render_template("bio.html")
+
+
+@app.route("/timeline")
 def timeline():
-    return render_template('timeline.html')
+    projects = [
+        {
+            "date": "07-01-2023",
+            "url": "https://github.com/Delpen9/Ian-Dover-Personal-Website/tree/main",
+            "title": "Portfolio Website",
+        },
+        {
+            "date": "06-25-2023",
+            "url": "https://github.com/Delpen9/Underwriting-Challenge",
+            "title": "Underwriting Challenge",
+        },
+        {
+            "date": "04-29-2023",
+            "url": "https://github.com/Delpen9/optimization_methods_final",
+            "title": "Optimization Methods",
+        },
+        {
+            "date": "04-20-2023",
+            "url": "https://github.com/Delpen9/data_compression_experiments",
+            "title": "Data Compression",
+        },
+        {
+            "date": "04-01-2023",
+            "url": "https://github.com/Delpen9/lasso_ridge_elastic_group_optimization",
+            "title": "Group Optimization",
+        },
+        {
+            "date": "03-20-2023",
+            "url": "https://github.com/Delpen9/Lagrangian-Optimization-Algorithms/tree/main",
+            "title": "Descent Algorithms",
+        },
+    ]
+    return render_template("timeline.html", projects=projects)
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5001))
-    app.run(debug = True, host = '0.0.0.0', port = port)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host="0.0.0.0", port=port)
