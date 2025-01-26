cd9fa6c5685279a86e86f3c843e8f4fcb2c9d951
Delpen9 <iantdover@gmail.com>
2025-01-25 19:29:43 -0600

    Bio improvements (#33)
    * improved bio
    
    * fixed issue with carousel
    
    * fixed navbar logic
    
    * added comment section
    
    * Add files via upload
    
    * added social media to the bio pages
    
    * fixed the bio page

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -227,5 +227,5 @@ def blog():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5003))
     app.run(debug=True, host="0.0.0.0", port=port)
